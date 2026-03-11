529e7449c6157ccebe82c48d27e56960ee71870f
Luis <luisoctaviogalessoseror@protonmail.com>
2026-03-11 17:52:44 -0400

    whitenoise

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from whitenoise import WhiteNoise
 from .routes import register_routes
 
 
 def create_app():
     app = Flask(__name__)
+    app.wsgi_app = WhiteNoise(
+        app.wsgi_app,
+        root="app/static/",
+        prefix="static/",
+    )
 
     register_routes(app)
 
